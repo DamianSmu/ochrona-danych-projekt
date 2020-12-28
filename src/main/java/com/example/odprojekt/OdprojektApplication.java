4915b04e3a599bc7b0166f74173055aa379d1625
damianSmu <smugorzewski.ds@gmail.com>
2020-12-28 16:00:10 +0100

    spring.io init

---
+++ src/main/java/com/example/odprojekt/OdprojektApplication.java
@@ -0,0 +1,13 @@
+package com.example.odprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdprojektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdprojektApplication.class, args);
+	}
+
+}
